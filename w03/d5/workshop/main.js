c76d909f39aeb70fc3cdac110759b58c33331744
Kevin Vahdat <kvahdat@nyc.yearup.org>
2018-11-09 12:50:29 -0500

    First commit workshop 5

--- w03/d5/workshop/main.js
+++ w03/d5/workshop/main.js
@@ -1,14 +1,27 @@
-class Art {
-  constructor(){
-
+class Classic {
+  constructor(frame, art_url) {
+    this.frame = frame;
+    this.art_url = art_url;
   }
+
+newElem = document.createElement('img');
+newElem.src = ""
+
+newElem.className = "pictures";
+
+let parent = document.getElementById('images');
+console.log(parent);
+parent.appendChild(newElem);
+
+let otherimage = document.createElement('img');
+
 
   render(){
     //do things to the DOM here
   }
 }
 
-class Classical extends Art {
+class Classic extends Art {
   constructor(){
     super();
   }
